475677a6c3c339bfb92810e6de42865640d33cc8
Hiroki Ishida <69500468+hiroki-ishida-0821@users.noreply.github.com>
2020-08-20 14:51:00 +0900

    portfolio/src
    folder upload

---
+++ src/mock/data.js
@@ -0,0 +1,130 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: '', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: '', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: '',
+  subtitle: '',
+  cta: '',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile.jpg',
+  paragraphOne: '',
+  paragraphTwo: '',
+  paragraphThree: '',
+  resume: '', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'product_01.jpg',
+    title: 'HYPER VSN',
+    info: 'ホログラムディスプレイ「HYPER VSN」のブランディングサイト',
+    info2: 'プロダクトのクールなイメージをベースに商品を紹介。簡単に見積もりを取れるシミュレーション機能を付け、コンバージョン率を意識した作りにした。',
+    url: 'https://3ddisplay.jp/',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'product_02.jpg',
+    title: '株式会社榮林',
+    info: '飲食店を多数展開する「株式会社榮林」様のコーポレートサイト',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'product_03.jpg',
+    title: 'グローバルキッズパーク',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'product_04.jpg',
+    title: 'AYA Counseling Room',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'product_05.jpg',
+    title: 'グローバルファーム',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'product_06.jpg',
+    title: '株式会社S-Grow',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'product_07.jpg',
+    title: '株式会社S-Grow | リクルート',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: '',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'twitter',
+      url: '',
+    },
+    {
+      id: nanoid(),
+      name: 'codepen',
+      url: '',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: '',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: '',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
